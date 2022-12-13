a7d1352d7d21c4728c7fd7321306c77f10f207e2
Kenny Thomson <kthom91@gmail.com>
2022-12-13 00:14:35 -0500

    initial nx workspace
    nx workspace setup and skeleton angular and nest apps

---
+++ packages/kt-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kt-monorepo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kt-ui';
+}
